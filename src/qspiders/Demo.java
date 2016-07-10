9db68aef89da63da4e1792a7022e63d7b0c6b157
RakhiC <rakhi.choukimath@gmail.com>
2016-07-10 14:24:08 +0530

    Update Demo.java

--- src/qspiders/Demo.java
+++ src/qspiders/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, Welcome to qspiders");
+			System.out.println("Learning is so much fun in Qspiders");
 			System.out.println("Thank you...");
 	}
 
